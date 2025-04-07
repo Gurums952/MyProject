3cb5aea3a2c21269ea6b351a039e2c3153f96150
Gurums952 <guru9171@gmail.com>
2025-04-07 13:21:32 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,20 @@
+let a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+for (let a=1 ; a <=15; a++) {
+        
+
+if (a%3==0 && a%5==0) {
+    console.log("FizzBuzz")
+}
+else if(a%3==0){
+    console.log("Fizz")
+}
+else if(a%5==0){
+    console.log("Buzz")
+}
+else{
+    console.log(a)
+}
+}
+ 
+    
+    
